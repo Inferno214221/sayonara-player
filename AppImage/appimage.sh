cf2b81052b1e0a002a548b4f85b23ace76f27ced
Lucio Carreras <luciocarreras@gmail.com>
2019-10-24 10:35:36 +0200

    #27: remove AppRun workaround

--- AppImage/appimage.sh
+++ AppImage/appimage.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-# run in docker container luciocarreras/sayonara-appimage
+# run in docker container luciocarreras/sayonara-ubuntu:xenial
 
 set -e
 
 case "$1" in
+
 	build)
 		mkdir -p build && cd build
 		cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DWITH_COTIRE=1 -DLINK_GSTREAMER_PLUGINS=1 -DCMAKE_PREFIX_PATH=/opt/qt512
 		make -j8
 		make install DESTDIR=AppDir
 		;;
+
 	deploy)
 		cd build
-		export EXTRA_QT_PLUGINS="iconengines,sqldrivers/libqsqlite.so,platforms/libqxcb.so" 
+
 		if [ "x${DOCKER_QT_BASE_DIR}" != "x" ] ; then
 			export QT_BASE_DIR=${DOCKER_QT_BASE_DIR}
 		else
@@ -25,14 +27,13 @@ case "$1" in
 		export PATH=$QT_BASE_DIR/bin:$PATH
 		export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/x86_64-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
 		export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
-		linuxdeploy-x86_64.AppImage --appdir=AppDir --desktop-file=./AppDir/usr/share/applications/sayonara.desktop --plugin=qt --custom-apprun=../AppImage/AppRun
+		export EXTRA_QT_PLUGINS="iconengines,sqldrivers/libqsqlite.so,platforms/libqxcb.so" 
 
-		# Workaround for https://github.com/linuxdeploy/linuxdeploy/issues/100
-		cp ../AppImage/AppRun AppDir/
-		chmod a+x AppDir/AppRun
+		linuxdeploy-x86_64.AppImage --appdir=AppDir --desktop-file=./AppDir/usr/share/applications/sayonara.desktop --plugin=qt --custom-apprun=../AppImage/AppRun
 
 		appimagetool-x86_64.AppImage AppDir
 		;;
+
 	*)
 		echo "Usage $0 build|deploy"
 		exit 1
