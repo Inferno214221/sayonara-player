9e13c5ab568b924d3fb0a36ba0dd175a01eb4e8c
Thomas Schläppi <tschlaeppi@hotmail.com>
2019-11-28 21:14:24 +0100

    Add prerequisites install script for building the snap package

---
+++ snap/local/install_prerequisites.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+: <<'Comments'
+
+This is a script for installing the prerequisites to build the snap package of the sayonara player
+
+Comments
+
+sudo snap install snapcraft --classic
+sudo snap install multipass --edge --classic
