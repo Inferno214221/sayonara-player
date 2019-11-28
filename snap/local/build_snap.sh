d3d4d9ec2fcbd14a875d8e005d5abfaeba9c5db1
Thomas Schläppi <tschlaeppi@hotmail.com>
2019-11-28 21:14:37 +0100

    Add buildscript for snap package

---
+++ snap/local/build_snap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+: <<'Comments'
+
+This is a script for building the snap package of the sayonara player
+cd into the repo root directory an launch snap/local/build_snap.sh
+
+Comments
+
+
+# get version from CMakeLists
+versionFinderRegex='\d+(?:\.\d+)+-(stable|beta)\d?'
+version=$(cat CMakeLists.txt | grep -oP $versionFinderRegex)
+
+if [[ -z "$version" ]]; then
+   echo "Could not find a version in CMakeLists.txt" 1>&2
+   exit 1
+fi
+
+commitid=$(git rev-parse --short HEAD)
+
+# replacing values in snapcraft.yaml
+sed -i "s/^version:.*$/version: ${version}-${commitid}/" snap/snapcraft.yaml
+
+# running snapcraft
+cd snap
+snapcraft clean
+snapcraft
