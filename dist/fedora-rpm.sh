d635e9dd40a025c10bfb00ee8ff56f43560dd1b0
Michael Lugmair <sayonara-player@posteo.org>
2020-12-29 14:10:33 +0100

    use latest fedora docker container

--- dist/fedora-rpm.sh
+++ dist/fedora-rpm.sh
@@ -4,52 +4,41 @@
 
 set -e
 
-case "$1" in
-	build-and-deploy)
-		BASE_DIR=${PWD}
+BASE_DIR=${PWD}
 
-		export SPEC_DIR="/root/rpmbuild/SPECS"
-		export SOURCES_DIR="/root/rpmbuild/SOURCES"
-		export RPM_DIR="/root/rpmbuild/RPMS"
+export SPEC_DIR="/root/rpmbuild/SPECS"
+export SOURCES_DIR="/root/rpmbuild/SOURCES"
+export RPM_DIR="/root/rpmbuild/RPMS"
 
-		rm -rf ${SOURCES_DIR} ${SPEC_DIR} ${RPM_DIR}
+rm -rf ${SOURCES_DIR} ${SPEC_DIR} ${RPM_DIR}
 
-		mkdir -p ${SOURCES_DIR}
-		mkdir -p ${SPEC_DIR}
-		mkdir -p ${RPM_DIR}
+mkdir -p ${SOURCES_DIR}
+mkdir -p ${SPEC_DIR}
+mkdir -p ${RPM_DIR}
 
-		cd ${SOURCES_DIR}
-		git clone https://gitlab.com/luciocarreras/sayonara-player.git sayonara-player
-		cd sayonara-player
+cd ${SOURCES_DIR}
+git clone https://gitlab.com/luciocarreras/sayonara-player.git sayonara-player
+cd sayonara-player
 
-		# create spec file
-		mkdir -p build && cd build
-		cmake ..
-		cd ../..
-		cp sayonara-player/build/dist/sayonara.spec ${SPEC_DIR}/
-		
-		rm -rf sayonara-player/debian
-		rm -rf sayonara-player/.git*
-		rm -rf sayonara-player/build
+# create spec file
+mkdir -p build && cd build
+cmake ..
+cd ../..
+cp sayonara-player/build/dist/sayonara.spec ${SPEC_DIR}/
 
-		VERSION=$(grep -oP '\d+(?:\.\d+)+-?\w+\d+' sayonara-player/CMakeLists.txt)
-		tar czf sayonara-player-${VERSION}.tar.gz sayonara-player
+rm -rf sayonara-player/debian
+rm -rf sayonara-player/.git*
+rm -rf sayonara-player/build
 
-		cd ${SPEC_DIR}
+VERSION=$(grep -oP '\d+(?:\.\d+)+-?\w+\d+' sayonara-player/CMakeLists.txt)
+tar czf sayonara-player-${VERSION}.tar.gz sayonara-player
 
-		# build
-		rpmbuild -ba sayonara.spec
+cd ${SPEC_DIR}
 
-		mkdir -p "${BASE_DIR}/rpms"
+# build
+rpmbuild -ba sayonara.spec
 
-		find ${RPM_DIR} -name "*.rpm" -exec cp -v {} "${BASE_DIR}/rpms/" \;
+mkdir -p "${BASE_DIR}/rpms"
 
-		;;
-	*)
-		echo "Usage: $0 build-and-deploy"
-		exit 1
-		;;
-esac
-
-exit 0
+find ${RPM_DIR} -name "*.rpm" -exec cp -v {} "${BASE_DIR}/rpms/" \;
 
