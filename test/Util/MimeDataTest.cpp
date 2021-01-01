73546dc895157f070f62605b39da539642c08d35
Michael Lugmair <sayonara-player@posteo.org>
2021-01-01 16:54:44 +0100

    add mimedata tests

---
+++ test/Util/MimeDataTest.cpp
@@ -0,0 +1,109 @@
+#include "SayonaraTest.h"
+// access working directory with Test::Base::tempPath("somefile.txt");
+
+#include "Gui/Utils/MimeData/CustomMimeData.h"
+
+#include "Utils/FileUtils.h"
+#include "Utils/MetaData/MetaData.h"
+#include "Utils/MetaData/MetaDataList.h"
+
+#include <QUrl>
+#include <QList>
+
+class MimeDataTest :
+	public Test::Base
+{
+	Q_OBJECT
+
+	public:
+		MimeDataTest() :
+			Test::Base("MimeDataTest")
+		{
+			Util::File::createDir(Test::Base::tempPath("directory1"));
+			Util::File::createDir(Test::Base::tempPath("directory2"));
+		}
+
+	private slots:
+		void testMetadata();
+		void testDirsNonUrl();
+		void testDirsUrl();
+};
+
+void MimeDataTest::testMetadata()
+{
+	auto mimeData = Gui::CustomMimeData(this);
+
+	MetaDataList tracks;
+	for(int i = 0; i < 10; i++)
+	{
+		auto md = MetaData(Test::Base::tempPath(QString("filename%1.mp3").arg(i)));
+		tracks << md;
+	}
+
+	mimeData.setMetadata(tracks);
+
+	QVERIFY(mimeData.hasMetadata());
+	QVERIFY(mimeData.metadata().size() == 10);
+
+	QVERIFY(mimeData.hasUrls());
+	QVERIFY(mimeData.urls().size() == 10);
+
+	const auto urls = mimeData.urls();
+	for(const auto& url : urls)
+	{
+		QVERIFY(Util::File::isUrl(url.toString()));
+	}
+}
+
+void MimeDataTest::testDirsNonUrl()
+{
+	auto mimeData = Gui::CustomMimeData(this);
+
+	const auto urls = QList<QUrl> {
+		QUrl(Test::Base::tempPath("directory1")),
+		QUrl(Test::Base::tempPath("directory2"))
+	};
+
+	mimeData.setUrls(urls);
+
+	QVERIFY(mimeData.hasUrls());
+	QVERIFY(mimeData.urls().size() == 2);
+	QVERIFY(mimeData.hasMetadata() == false);
+
+	const auto fetchedUrls = mimeData.urls();
+	for(const auto& url : fetchedUrls)
+	{
+		QVERIFY(!Util::File::isUrl(url.toString()));
+	}
+}
+
+void MimeDataTest::testDirsUrl()
+{
+	auto mimeData = Gui::CustomMimeData(this);
+
+	auto urls = QList<QUrl> {
+		QUrl(Test::Base::tempPath("directory1")),
+		QUrl(Test::Base::tempPath("directory2"))
+	};
+
+	for(auto& url : urls)
+	{
+		url.setScheme("file");
+	}
+
+	mimeData.setUrls(urls);
+
+	QVERIFY(mimeData.hasUrls());
+	QVERIFY(mimeData.urls().size() == 2);
+	QVERIFY(mimeData.hasMetadata() == false);
+
+	const auto fetchedUrls = mimeData.urls();
+	for(const auto& url : fetchedUrls)
+	{
+		QVERIFY(Util::File::isUrl(url.toString()));
+	}
+}
+
+QTEST_GUILESS_MAIN(MimeDataTest)
+
+#include "MimeDataTest.moc"
