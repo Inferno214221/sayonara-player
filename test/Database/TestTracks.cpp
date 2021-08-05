d67f9255c4bec50c192a58684e4ae27ea5f3b7e4
Michael Lugmair <sayonara-player@posteo.org>
2021-08-05 23:42:21 +0200

    add unit tests for tracks db

---
+++ test/Database/TestTracks.cpp
@@ -0,0 +1,164 @@
+/* TestDatabase.cpp */
+/*
+ * Copyright (C) 2011-2021 Michael Lugmair
+ *
+ * This file is part of sayonara player
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Database/TestTracks.h"
+#include "Utils/MetaData/MetaDataList.h"
+#include "Utils/MetaData/MetaData.h"
+#include "Utils/Utils.h"
+
+#include <QDateTime>
+
+namespace
+{
+	MetaDataList createMilkyWayTracks()
+	{
+		MetaDataList result;
+
+		const auto artist = "";
+		{
+			const auto album = "Solar System";
+			result << Test::createTrack(1, "Mercury", artist, album);
+			result << Test::createTrack(2, "Venus", artist, album);
+			result << Test::createTrack(3, "Earth", artist, album);
+			result << Test::createTrack(4, "Mars", artist, album);
+			result << Test::createTrack(5, "Jupiter", artist, album);
+			result << Test::createTrack(6, "Saturn", artist, album);
+			result << Test::createTrack(7, "Neptune", artist, album);
+			result << Test::createTrack(8, "Uranus", artist, album);
+		}
+
+		return result;
+	}
+
+	MetaDataList createOneYearTracks()
+	{
+		MetaDataList result;
+
+		const auto artist = "One Year";
+		{
+			const auto album = "Spring";
+			result << Test::createTrack(1, "March", artist, album);
+			result << Test::createTrack(2, "April", artist, album);
+			result << Test::createTrack(3, "May", artist, album);
+		}
+
+		{
+			const auto album = "Summer";
+			result << Test::createTrack(1, "June", artist, album);
+			result << Test::createTrack(2, "July", artist, album);
+			result << Test::createTrack(3, "August", artist, album);
+		}
+
+		{
+			const auto album = "Fall";
+			result << Test::createTrack(1, "September", artist, album);
+			result << Test::createTrack(2, "October", artist, album);
+			result << Test::createTrack(3, "November", artist, album);
+		}
+
+		{
+			const auto album = "Winter";
+			result << Test::createTrack(1, "December", artist, album);
+			result << Test::createTrack(2, "January", artist, album);
+			result << Test::createTrack(3, "Feburaty", artist, album);
+		}
+
+		return result;
+	}
+
+	MetaDataList createEarthTracks()
+	{
+		MetaDataList result;
+
+		const auto commonArtist = "Earth";
+		{
+			const auto album = "Asia Rivers";
+			const auto albumArtist = "Asia";
+			const auto artist = QString("%1 feat. %2").arg(commonArtist).arg(albumArtist);
+			result << Test::createTrack(1, "Yang Tse", artist, album, albumArtist);
+			result << Test::createTrack(2, "Mekong", artist, album, albumArtist);
+			result << Test::createTrack(3, "Shinano", artist, album, albumArtist);
+		}
+
+		{
+			const auto album = "Europe Mountains";
+			const auto albumArtist = "Europe";
+			const auto artist = QString("%1 feat. %2").arg(commonArtist).arg(albumArtist);
+			result << Test::createTrack(1, "Zugspitze", artist, album, albumArtist);
+			result << Test::createTrack(2, "Mont Blanc", artist, album, albumArtist);
+			result << Test::createTrack(3, "Seine", artist, album, albumArtist);
+			result << Test::createTrack(4, "Matterhorn", artist, album, albumArtist);
+		}
+
+		{
+			const auto album = "African Countries";
+			const auto albumArtist = "Africa";
+			const auto artist = QString("%1 feat. %2").arg(commonArtist).arg(albumArtist);
+			result << Test::createTrack(1, "Algeria", artist, album, albumArtist);
+			result << Test::createTrack(2, "Mosambique", artist, album, albumArtist);
+			result << Test::createTrack(3, "Ivory Coast", artist, album, albumArtist);
+			result << Test::createTrack(4, "Kenia", artist, album, albumArtist);
+			result << Test::createTrack(5, "Nigeria", artist, album, albumArtist);
+		}
+
+		return result;
+	}
+}
+
+namespace Test
+{
+	MetaDataList createTracks()
+	{
+		return createMilkyWayTracks() << createEarthTracks() << createOneYearTracks();
+	}
+
+	MetaData
+	createTrack(const uint16_t trackNumber, const QString& title, const QString& artist, const QString& album,
+	            const QString& albumArtist)
+	{
+		const auto filepath = QString("/path/to/%1/%2/%3.mp3")
+			.arg(artist)
+			.arg(album)
+			.arg(title);
+
+		MetaData track;
+		track.setTrackNumber(trackNumber);
+		track.setTitle(title);
+		track.setArtist(artist);
+		track.setAlbumArtist(albumArtist);
+		track.setAlbum(album);
+		track.setFilepath(filepath);
+		track.setGenres(QStringList() << artist << album);
+
+		const auto currentDate = Util::dateToInt(QDateTime::currentDateTime());
+
+		track.setBitrate(320);
+		track.setCreatedDate(currentDate);
+		track.setModifiedDate(currentDate);
+		track.setDiscnumber(1);
+		track.setDurationMs(10'000);
+		track.setComment("Some comment");
+		track.setFilesize(100'000);
+		track.setRating(Rating::Five);
+		track.setYear(2000);
+
+		return track;
+	}
+}
---
+++ test/Database/TracksTest.cpp
@@ -0,0 +1,420 @@
+/* TestDatabase.cpp */
+/*
+ * Copyright (C) 2011-2021 Michael Lugmair
+ *
+ * This file is part of sayonara player
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "SayonaraTest.h"
+#include "TestTracks.h"
+#include "Database/Connector.h"
+#include "Database/LibraryDatabase.h"
+
+#include "Utils/Algorithm.h"
+#include "Utils/MetaData/MetaDataList.h"
+#include "Utils/MetaData/Artist.h"
+#include "Utils/MetaData/Album.h"
+#include "Utils/MetaData/Genre.h"
+#include "Utils/Library/Filter.h"
+#include "Utils/Set.h"
+#include "Utils/Settings/Settings.h"
+
+namespace
+{
+	constexpr const auto SearchMode = (Library::SearchMode::CaseInsensitve | Library::SearchMode::NoSpecialChars);
+
+	bool findTrack(const MetaData& track, const MetaDataList& tracks)
+	{
+		return Util::Algorithm::contains(tracks, [&](const auto& element) {
+			return (element.filepath() == track.filepath());
+		});
+	}
+
+	bool isSubset(const MetaDataList& needle, const MetaDataList& hayStack)
+	{
+		return std::all_of(std::begin(needle), std::end(needle), [&](const auto& track) {
+			return findTrack(track, hayStack);
+		});
+	}
+
+	Library::Filter createFulltextFilter(const QString& str)
+	{
+		Library::Filter filter;
+		filter.setFiltertext(str, SearchMode);
+		filter.setMode(Library::Filter::Mode::Fulltext);
+
+		return filter;
+	}
+}
+
+class TracksTest :
+	public Test::Base
+{
+	Q_OBJECT
+
+	public:
+		TracksTest() :
+			Test::Base("TracksTest")
+		{
+			m_testTracks = Test::createTracks();
+			Settings::instance()->set<Set::Lib_SearchMode>(SearchMode);
+		}
+
+	private slots:
+		void testGetAllTracks();
+		void testFetchByArtist();
+		void testFetchByArtistWithFilter();
+		void testFetchByAlbumArtist();
+		void testFetchByAlbumArtistWithFilter();
+		void testFetchByAlbum();
+		void testFetchByAlbumWithFilter();
+		void testSearchByFulltext();
+		void testSearchByGenre();
+		void testSearchByFilepath();
+		void testGetAllGenres();
+		void testGetByPaths();
+		void testInsertAndUpdate();
+		void testInsertAndDelete();
+		void testRenameFilepath();
+
+	private:
+		DB::LibraryDatabase*
+		initDatabase(DB::LibraryDatabase::ArtistIDField artistIdField = DB::LibraryDatabase::ArtistIDField::ArtistID)
+		{
+			if(m_libraryDatabase)
+			{
+				m_libraryDatabase->changeArtistIdField(artistIdField);
+				return m_libraryDatabase;
+			}
+
+			auto* db = DB::Connector::instance();
+			db->registerLibraryDatabase(0);
+			m_libraryDatabase = db->libraryDatabase(0, 0);
+			m_libraryDatabase->storeMetadata(m_testTracks);
+			m_libraryDatabase->updateSearchMode();
+			m_libraryDatabase->changeArtistIdField(artistIdField);
+
+			return m_libraryDatabase;
+		}
+
+		DB::LibraryDatabase* m_libraryDatabase = nullptr;
+		MetaDataList m_testTracks;
+};
+
+void TracksTest::testGetAllTracks()
+{
+	auto* db = initDatabase();
+	MetaDataList tracks;
+	db->getAllTracks(tracks);
+
+	QVERIFY(tracks.size() == m_testTracks.size());
+	QVERIFY(isSubset(tracks, m_testTracks));
+}
+
+void TracksTest::testFetchByAlbumArtist()
+{
+	auto* db = initDatabase(DB::LibraryDatabase::ArtistIDField::AlbumArtistID);
+
+	MetaDataList tracks;
+	const auto artistId = db->getArtistID("Europe");
+	db->getAllTracksByArtist({artistId}, tracks);
+
+	QVERIFY(tracks.size() == 4);
+	QVERIFY(isSubset(tracks, m_testTracks));
+}
+
+void TracksTest::testFetchByAlbumArtistWithFilter()
+{
+	auto* db = initDatabase(DB::LibraryDatabase::ArtistIDField::AlbumArtistID);
+
+	MetaDataList tracks;
+	const auto artistId = db->getArtistID("Africa");
+	const auto filter = createFulltextFilter("earth");
+
+	db->getAllTracksByArtist({artistId}, tracks, filter);
+
+	QVERIFY(tracks.size() == 5);
+	QVERIFY(isSubset(tracks, m_testTracks));
+}
+
+void TracksTest::testFetchByArtist()
+{
+	auto* db = initDatabase();
+
+	MetaDataList tracks;
+	const auto artistId = db->getArtistID("Earth feat. Europe");
+	db->getAllTracksByArtist({artistId}, tracks);
+
+	QVERIFY(tracks.size() == 4);
+	QVERIFY(isSubset(tracks, m_testTracks));
+}
+
+void TracksTest::testFetchByArtistWithFilter()
+{
+	auto* db = initDatabase();
+
+	MetaDataList tracks;
+	const auto artistId = db->getArtistID("Earth feat. Europe");
+	const auto filter = createFulltextFilter("earth");
+
+	db->getAllTracksByArtist({artistId}, tracks, filter);
+
+	QVERIFY(tracks.size() == 4);
+	QVERIFY(isSubset(tracks, m_testTracks));
+}
+
+void TracksTest::testFetchByAlbum()
+{
+	auto* db = initDatabase();
+
+	MetaDataList tracks;
+	const auto albumId = db->getAlbumID("Europe Mountains");
+	db->getAllTracksByAlbum({albumId}, tracks);
+
+	QVERIFY(tracks.size() == 4);
+	QVERIFY(isSubset(tracks, m_testTracks));
+}
+
+void TracksTest::testFetchByAlbumWithFilter()
+{
+	auto* db = initDatabase();
+
+	MetaDataList tracks;
+	const auto albumId = db->getAlbumID("Europe Mountains");
+	const auto filter = createFulltextFilter("earth");
+
+	db->getAllTracksByAlbum({albumId}, tracks, filter, -1);
+
+	QVERIFY(tracks.size() == 4);
+	QVERIFY(isSubset(tracks, m_testTracks));
+}
+
+void TracksTest::testSearchByFulltext()
+{
+	{ // by album "African Countries"
+		auto* db = initDatabase();
+		const auto filter = createFulltextFilter("africancountri");
+
+		MetaDataList tracks;
+		db->getAllTracksBySearchString(filter, tracks);
+
+		QVERIFY(tracks.size() == 5);
+		QVERIFY(isSubset(tracks, m_testTracks));
+	}
+
+	{ // by artist "earth feat. *"
+		auto* db = initDatabase();
+		const auto filter = createFulltextFilter("earthfeat");
+
+		MetaDataList tracks;
+		db->getAllTracksBySearchString(filter, tracks);
+
+		QVERIFY(tracks.size() == 12);
+		QVERIFY(isSubset(tracks, m_testTracks));
+	}
+
+	{ // by title "Matterhorn"
+		auto* db = initDatabase();
+		const auto filter = createFulltextFilter("atterhor");
+
+		MetaDataList tracks;
+		db->getAllTracksBySearchString(filter, tracks);
+
+		QVERIFY(tracks.size() == 1);
+		QVERIFY(isSubset(tracks, m_testTracks));
+	}
+}
+
+void TracksTest::testSearchByGenre()
+{
+	Library::Filter filter;
+	filter.setFiltertext("asiarivers", SearchMode);
+	filter.setMode(Library::Filter::Mode::Genre);
+
+	auto* db = initDatabase();
+
+	MetaDataList tracks;
+	db->getAllTracksBySearchString(filter, tracks);
+
+	QVERIFY(tracks.size() == 3);
+	QVERIFY(isSubset(tracks, m_testTracks));
+}
+
+void TracksTest::testSearchByFilepath()
+{
+	Library::Filter filter;
+	filter.setFiltertext("rivers", SearchMode);
+	filter.setMode(Library::Filter::Mode::Filename);
+
+	auto* db = initDatabase();
+
+	MetaDataList tracks;
+	db->getAllTracksBySearchString(filter, tracks);
+
+	QVERIFY(tracks.size() == 3);
+	QVERIFY(isSubset(tracks, m_testTracks));
+}
+
+void TracksTest::testGetAllGenres()
+{
+	auto* db = initDatabase();
+
+	ArtistList artists;
+	AlbumList albums;
+	db->getAllArtists(artists, false);
+	db->getAllAlbums(albums, false);
+
+	const auto genres = db->getAllGenres();
+	QVERIFY(genres.size() == (artists.size() + albums.size()));
+}
+
+void TracksTest::testGetByPaths()
+{
+	QStringList paths;
+	for(int i = 0; i < 7; i++)
+	{
+		paths << m_testTracks[i].filepath();
+	}
+
+	QStringList incompletePaths;
+	for(int i = 8; i < 10; i++)
+	{
+		const auto path = m_testTracks[i].filepath();
+		incompletePaths << path.left(path.size() - 3);
+	}
+
+	paths.append(incompletePaths);
+
+	auto* db = initDatabase();
+	{
+		MetaDataList tracks;
+		db->getMultipleTracksByPath(paths, tracks);
+
+		QVERIFY(tracks.count() == (paths.count() - incompletePaths.size()));
+	}
+
+	{
+		MetaDataList tracks;
+		db->getAllTracksByPaths(paths, tracks);
+
+		QVERIFY(tracks.count() == paths.count());
+	}
+}
+
+void TracksTest::testInsertAndUpdate()
+{
+	auto* db = initDatabase();
+
+	const auto originalTrack = Test::createTrack(1, "title", "artist", "album", "albumArtist");
+	const auto path = originalTrack.filepath();
+	const auto success = db->storeMetadata(MetaDataList {originalTrack});
+	QVERIFY(success);
+
+	{
+		auto track = db->getTrackByPath(path);
+		QVERIFY(track.id() >= 0);
+		QVERIFY(track.title() == "title");
+		QVERIFY(track.artist() == "artist");
+		QVERIFY(track.album() == "album");
+		QVERIFY(track.albumArtist() == "albumArtist");
+
+		track.setBitrate(128'000);
+		track.setComment("newComment");
+		track.setDiscnumber(5);
+		track.setFilesize(200'000);
+		track.setGenres(QStringList() << "newGenre1" << "newGenre2");
+		track.setDurationMs(200'000);
+		track.setRating(Rating::One);
+		track.setTitle("newTitle");
+		track.setTrackNumber(2);
+		track.setYear(1994);
+
+		const auto success = db->updateTrack(track);
+		QVERIFY(success);
+	}
+
+	{
+		const auto track = db->getTrackByPath(path);
+		QVERIFY(track.id() >= 0);
+		QVERIFY(track.bitrate() == 128'000);
+		QVERIFY(track.comment() == "newComment");
+		QVERIFY(track.discnumber() == 5);
+		QVERIFY(track.filesize() == 200'000);
+		QVERIFY(track.genres().size() == 2);
+		QVERIFY(track.genres().contains(Genre("newGenre1")));
+		QVERIFY(track.genres().contains(Genre("newGenre2")));
+		QVERIFY(track.durationMs() == 200'000);
+		QVERIFY(track.rating() == Rating::One);
+		QVERIFY(track.title() == "newTitle");
+		QVERIFY(track.trackNumber() == 2);
+		QVERIFY(track.year() == 1994);
+	}
+}
+
+void TracksTest::testInsertAndDelete()
+{
+	auto* db = initDatabase();
+
+	const auto originalTrack =
+		Test::createTrack(1, "titleToDelete", "artistToDelete", "albumToDelete", "albumArtistToDelete");
+	const auto path = originalTrack.filepath();
+	const auto success = db->storeMetadata(MetaDataList {originalTrack});
+	QVERIFY(success);
+
+	{
+		const auto track = db->getTrackByPath(path);
+		QVERIFY(track.id() >= 0);
+		QVERIFY(!track.isEqualDeep(MetaData {}));
+		db->deleteTrack(track.id());
+	}
+
+	{
+		const auto deletedTrack = db->getTrackByPath(path);
+		QVERIFY(deletedTrack.id() < 0);
+		QVERIFY(deletedTrack.isEqualDeep(MetaData {}));
+	}
+}
+
+void TracksTest::testRenameFilepath()
+{
+	auto* db = initDatabase();
+
+	const auto originalTrack =
+		Test::createTrack(1, "titleToRename", "artistToRename", "albumToRename", "albumArtistToRename");
+	const auto path = originalTrack.filepath();
+	const auto newPath = QStringLiteral("/some/new/path.mp3");
+	const auto success = db->storeMetadata(MetaDataList {originalTrack});
+	QVERIFY(success);
+
+	{
+		const auto track = db->getTrackByPath(path);
+		QVERIFY(track.id() >= 0);
+		db->renameFilepath(path, newPath, track.libraryId());
+	}
+
+	{
+		const auto nonExistentTrack = db->getTrackByPath(path);
+		QVERIFY(nonExistentTrack.isEqualDeep(MetaData {}));
+
+		const auto trackWithNewPath = db->getTrackByPath(newPath);
+		QVERIFY(trackWithNewPath.id() >= 0);
+		QVERIFY(trackWithNewPath.filepath() == newPath);
+	}
+}
+
+QTEST_GUILESS_MAIN(TracksTest)
+
+#include "TracksTest.moc"
