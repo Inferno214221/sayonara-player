d9fa062e55a45d8605deeb7611b2acedbca55efc
Michael Lugmair <sayonara-player@posteo.org>
2022-06-02 21:16:55 +0200

    create smart playlists tests

---
+++ test/SmartPlaylists/ConverterTest.cpp
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) 2011-2022 Michael Lugmair
+ *
+ * This file is part of sayonara player
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Common/SayonaraTest.h"
+
+#include "Components/SmartPlaylists/DateConverter.h"
+#include "Components/SmartPlaylists/StringConverter.h"
+#include "Components/SmartPlaylists/TimeSpanConverter.h"
+
+#include <array>
+#include <utility>
+
+// access working directory with Test::Base::tempPath("somefile.txt");
+
+namespace
+{
+	using OptionalInt = std::optional<int>;
+
+	constexpr const auto DaysPerYear = 365;
+	constexpr const auto DaysPerMonth = 30;
+
+	auto tupleToInt(const int years, const int months, const int days)
+	{
+		return years * DaysPerYear + months * DaysPerMonth + days;
+	}
+}
+
+using namespace SmartPlaylists;
+
+class ConverterTest :
+	public Test::Base
+{
+	Q_OBJECT
+
+	public:
+		ConverterTest() :
+			Test::Base("ConverterTest") {}
+
+	private slots:
+		void testDateConverter();
+		void testStringConverter();
+		void testTimeSpanConverter();
+};
+
+void ConverterTest::testDateConverter()
+{
+	const auto converter = DateConverter();
+	const auto testCasesIntToString = std::array {
+		std::make_pair(20091214, QString("2009-12-14")),
+		std::make_pair(2056487, QString()),
+		std::make_pair(204547858, QString()),
+		std::make_pair(0, QString())
+	};
+
+	for(const auto& [value, expected]: testCasesIntToString)
+	{
+		QVERIFY(converter.intToString(value) == expected);
+	}
+
+	const auto testCasesStringToInt = std::array<std::pair<QString, OptionalInt>, 4> {
+		std::make_pair(QString("2009-12-14"), 20091214),
+		std::make_pair(QString(), std::nullopt),
+		std::make_pair(QString("Invalid String"), std::nullopt),
+		std::make_pair(QString("9876-54-32"), std::nullopt)
+	};
+
+	for(const auto& [value, expected]: testCasesStringToInt)
+	{
+		QVERIFY(converter.stringToInt(value) == expected);
+	}
+}
+
+void ConverterTest::testStringConverter()
+{
+	const auto converter = StringConverter();
+	const auto testCasesIntToString = std::array {
+		std::make_pair(20091214, "20091214"),
+		std::make_pair(-2, "-2"),
+		std::make_pair(204547858, "204547858"),
+		std::make_pair(0, "0")
+	};
+
+	for(const auto& [value, expected]: testCasesIntToString)
+	{
+		QVERIFY(converter.intToString(value) == expected);
+	}
+
+	const auto testCasesStringToInt = std::array<std::pair<QString, OptionalInt>, 5> {
+		std::make_pair(QString("20091214"), std::optional {20091214}),
+		std::make_pair(QString("-2"), std::optional {-2}),
+		std::make_pair(QString(), std::nullopt),
+		std::make_pair(QString("9876-54-32"), std::nullopt),
+		std::make_pair(QString("Invalid"), std::nullopt)
+	};
+
+	for(const auto& [value, expected]: testCasesStringToInt)
+	{
+		QVERIFY(converter.stringToInt(value) == expected);
+	}
+}
+
+void ConverterTest::testTimeSpanConverter()
+{
+	const auto converter = TimeSpanConverter();
+
+	const auto testCasesIntToString = std::array {
+		std::make_pair(tupleToInt(0, 0, 0), QString("0")),
+		std::make_pair(tupleToInt(2, 0, 0), QString("2y")),
+		std::make_pair(tupleToInt(0, 3, 0), QString("3m")),
+		std::make_pair(tupleToInt(0, 0, 4), QString("4d")),
+		std::make_pair(tupleToInt(2, 3, 0), QString("2y, 3m")),
+		std::make_pair(tupleToInt(0, 3, 4), QString("3m, 4d")),
+		std::make_pair(tupleToInt(2, 0, 4), QString("2y, 4d")),
+		std::make_pair(tupleToInt(2, 0, DaysPerYear * 2 + 1), QString("4y, 1d")),
+		std::make_pair(tupleToInt(0, 0, DaysPerMonth * 3 + 1), QString("3m, 1d")),
+		std::make_pair(tupleToInt(0, 25, 4), QString("2y, 24d")),
+	};
+
+	for(const auto& [value, expected]: testCasesIntToString)
+	{
+		QVERIFY(converter.intToString(value) == expected);
+	}
+
+	const auto testCasesStringToInt = std::array<std::pair<QString, OptionalInt>, 8> {
+		std::make_pair(QString(), std::nullopt),
+		std::make_pair(QString("3y, 2m, 1d"), 3 * DaysPerYear + 2 * DaysPerMonth + 1),
+		std::make_pair(QString("3y 2m 1d"), 3 * DaysPerYear + 2 * DaysPerMonth + 1),
+		std::make_pair(QString("3, 2, 1"), std::nullopt),
+		std::make_pair(QString("3 2 1"), std::nullopt),
+		std::make_pair(QString("2m, 365"), 1 * DaysPerYear + 2 * DaysPerMonth),
+		std::make_pair(QString("2m, 365x"), std::nullopt),
+		std::make_pair(QString("1y, 2y"), std::nullopt)
+	};
+
+	for(const auto& [value, expected]: testCasesStringToInt)
+	{
+		QVERIFY(converter.stringToInt(value) == expected);
+	}
+}
+
+QTEST_GUILESS_MAIN(ConverterTest)
+
+#include "ConverterTest.moc"
---
+++ test/SmartPlaylists/SmartPlaylistManagerTest.cpp
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) 2011-2022 Michael Lugmair
+ *
+ * This file is part of sayonara player
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Common/SayonaraTest.h"
+#include "Common/PlayManagerMock.h"
+
+#include "Components/SmartPlaylists/SmartPlaylistManager.h"
+#include "Components/SmartPlaylists/SmartPlaylistByYear.h"
+#include "Components/SmartPlaylists/SmartPlaylistByRating.h"
+
+#include "Components/Playlist/Playlist.h"
+
+#include "Interfaces/PlaylistInterface.h"
+
+// access working directory with Test::Base::tempPath("somefile.txt");
+
+class PlaylistCreatorMock :
+	public PlaylistCreator
+{
+	public:
+		~PlaylistCreatorMock() override = default;
+
+		PlaylistPtr playlist(int playlistIndex) override { return mPlaylists[playlistIndex]; }
+
+		PlaylistPtr playlistById(int /*playlistId*/) override { return {}; }
+
+		[[nodiscard]] QString requestNewPlaylistName(const QString& /*prefix*/) const override { return {}; }
+
+		int createPlaylist(const MetaDataList& /*tracks*/, const QString& name, bool /*temporary*/) override
+		{
+			auto playlist = std::make_shared<Playlist::Playlist>(mPlaylists.count(), name, new PlayManagerMock());
+			mPlaylists.push_back(playlist);
+			return mPlaylists.count() - 1;
+		}
+
+		int createPlaylist(const QStringList& /*pathList*/, const QString& /*name*/,
+		                   bool /*temporary*/) override { return 0; }
+
+		int createPlaylist(const CustomPlaylist& /*customPlaylist*/) override { return 0; }
+
+		int createEmptyPlaylist(bool /*override*/) override { return 0; }
+
+		int createCommandLinePlaylist(const QStringList& /*pathList*/) override { return 0; }
+
+		[[nodiscard]] QList<PlaylistPtr> playlists() const { return mPlaylists; }
+
+	private:
+		QList<PlaylistPtr> mPlaylists;
+};
+
+class SmartPlaylistManagerTest :
+	public Test::Base
+{
+	Q_OBJECT
+
+	public:
+		SmartPlaylistManagerTest() :
+			Test::Base("SmartPlaylistManagerTest") {}
+
+	private slots:
+		[[maybe_unused]] void testInsert();
+		[[maybe_unused]] void testEdit();
+		[[maybe_unused]] void testDelete();
+		[[maybe_unused]] void testSelect();
+};
+
+[[maybe_unused]] void SmartPlaylistManagerTest::testInsert()
+{
+	auto manager = SmartPlaylistManager(new PlaylistCreatorMock());
+	auto smartPlaylist1 = std::make_shared<SmartPlaylistByYear>(-1, 2000, 2011);
+	auto smartPlaylist2 = std::make_shared<SmartPlaylistByRating>(-1, 1, 4);
+
+	QVERIFY(manager.smartPlaylists().count() == 0);
+
+	manager.insertPlaylist(smartPlaylist1);
+	manager.insertPlaylist(smartPlaylist2);
+
+	QVERIFY(manager.smartPlaylist(0)->name() == smartPlaylist1->name());
+	QVERIFY(manager.smartPlaylist(0)->id() >= 0);
+	QVERIFY(manager.smartPlaylist(0)->from() == 2000);
+	QVERIFY(manager.smartPlaylist(0)->to() == 2011);
+
+	QVERIFY(manager.smartPlaylist(1)->name() == smartPlaylist2->name());
+	QVERIFY((manager.smartPlaylist(1)->id() > 0) && (manager.smartPlaylist(1)->id() != manager.smartPlaylist(0)->id()));
+	QVERIFY(manager.smartPlaylist(1)->from() == 1);
+	QVERIFY(manager.smartPlaylist(1)->to() == 4);
+
+	QVERIFY(manager.smartPlaylists().count() == 2);
+
+	auto newManager = SmartPlaylistManager(new PlaylistCreatorMock());
+	QVERIFY(newManager.smartPlaylists().count() == manager.smartPlaylists().count());
+	QVERIFY(newManager.smartPlaylist(0)->id() == manager.smartPlaylist(0)->id());
+	QVERIFY(newManager.smartPlaylist(1)->id() == manager.smartPlaylist(1)->id());
+
+	manager.deletePlaylist(0);
+	manager.deletePlaylist(0);
+}
+
+void SmartPlaylistManagerTest::testEdit()
+{
+	auto manager = SmartPlaylistManager(new PlaylistCreatorMock());
+	auto smartPlaylist = std::make_shared<SmartPlaylistByYear>(-1, 2002, 2011);
+
+	manager.insertPlaylist(smartPlaylist);
+	QVERIFY(manager.smartPlaylists().count() == 1);
+
+	const auto oldName = smartPlaylist->name();
+	const auto oldId = manager.smartPlaylist(0)->id();
+
+	auto newSmartPlaylist = manager.smartPlaylist(0);
+	newSmartPlaylist->setFrom(2005);
+	newSmartPlaylist->setTo(2015);
+
+	manager.updatePlaylist(0, newSmartPlaylist);
+
+	QVERIFY(manager.smartPlaylist(0)->name() != oldName);
+	QVERIFY(manager.smartPlaylist(0)->name() == newSmartPlaylist->name());
+	QVERIFY(manager.smartPlaylist(0)->id() == oldId);
+	QVERIFY(manager.smartPlaylist(0)->from() == 2005);
+	QVERIFY(manager.smartPlaylist(0)->to() == 2015);
+
+	QVERIFY(manager.smartPlaylists().count() == 1);
+
+	auto newManager = SmartPlaylistManager(new PlaylistCreatorMock());
+	QVERIFY(newManager.smartPlaylists().count() == manager.smartPlaylists().count());
+	QVERIFY(newManager.smartPlaylist(0)->id() == manager.smartPlaylist(0)->id());
+	QVERIFY(newManager.smartPlaylist(0)->name() == manager.smartPlaylist(0)->name());
+
+	manager.deletePlaylist(0);
+}
+
+void SmartPlaylistManagerTest::testDelete()
+{
+	auto manager = SmartPlaylistManager(new PlaylistCreatorMock());
+	auto smartPlaylist = std::make_shared<SmartPlaylistByYear>(-1, 2003, 2011);
+
+	manager.insertPlaylist(smartPlaylist);
+	manager.deletePlaylist(0);
+
+	QVERIFY(manager.smartPlaylists().isEmpty());
+
+	auto newManager = SmartPlaylistManager(new PlaylistCreatorMock());
+	QVERIFY(newManager.smartPlaylists().count() == manager.smartPlaylists().count());
+}
+
+void SmartPlaylistManagerTest::testSelect()
+{
+	auto* playlistCreator = new PlaylistCreatorMock();
+	auto manager = SmartPlaylistManager(playlistCreator);
+	auto smartPlaylist = std::make_shared<SmartPlaylistByYear>(-1, 2003, 2011);
+
+	manager.insertPlaylist(smartPlaylist);
+	manager.selectPlaylist(0);
+
+	const auto playlists = playlistCreator->playlists();
+	QVERIFY(playlists.count() == 1);
+	QVERIFY(playlists[0]->name() == smartPlaylist->name());
+}
+
+QTEST_GUILESS_MAIN(SmartPlaylistManagerTest)
+
+#include "SmartPlaylistManagerTest.moc"
