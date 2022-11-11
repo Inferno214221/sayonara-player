997599784c054ef99f7306e705413d508ddf07d4
Michael Lugmair <sayonara-player@posteo.org>
2022-11-12 00:12:38 +0100

    add new mocks in tests
    ArchiveExtractor, FileSystem and DirectoryReader

--- test/NewTest.cmake
+++ test/NewTest.cmake
@@ -2,7 +2,14 @@ function(NEW_TEST ...)
 	string(REGEX MATCH "^(.*/)*(.*)\\.[^.]*$" dummy ${ARGV0})
 	set(TEST_NAME ${CMAKE_MATCH_2})
 
-	add_executable(${TEST_NAME} Common/SayonaraTest.cpp Common/PlayManagerMock.h ${ARGV} ${RESOURCES_RCC})
+	add_executable(${TEST_NAME}
+			Common/SayonaraTest.cpp
+			Common/PlayManagerMock.h
+			Common/FileSystemMock.cpp
+			Common/DirectoryReaderMock.cpp
+			Common/TagReaderMock.cpp
+			${ARGV} ${RESOURCES_RCC})
+
 	target_link_libraries(${TEST_NAME}
 		sayonara_components
 		Qt5::Test
