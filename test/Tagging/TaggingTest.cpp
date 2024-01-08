4a6f87a379e939e948d75f30faae47c198d93481
Michael Lugmair <sayonara-player@posteo.org>
2024-01-08 01:09:10 +0100

    Tagging: add selective Tagging test

--- test/Tagging/TaggingTest.cpp
+++ test/Tagging/TaggingTest.cpp
@@ -54,6 +54,20 @@ namespace
 		private:
 			QString m_targetFile;
 	};
+
+	MetaData createBasicTrack(const QString& filename)
+	{
+		auto track = MetaData(filename);
+		track.setArtist("artist");
+		track.setAlbum("album");
+		track.setGenres(QStringList() << "genre1" << "genre2");
+		track.setTitle("title");
+		track.setComment("comment");
+		track.setYear(1995);
+		track.setTrackNumber(17);
+
+		return track;
+	}
 }
 
 class TaggingTest :
@@ -85,14 +99,7 @@ class TaggingTest :
 
 			for(const auto& testCase: testCases)
 			{
-				auto track = MetaData(testCase.testEnv.filename());
-				track.setArtist("artist");
-				track.setAlbum("album");
-				track.setGenres(QStringList() << "genre1" << "genre2");
-				track.setTitle("title");
-				track.setComment("comment");
-				track.setYear(1995);
-				track.setTrackNumber(17);
+				const auto track = createBasicTrack(testCase.testEnv.filename());
 
 				const auto success = Tagging::Utils::setMetaDataOfFile(track);
 				QVERIFY(success);
@@ -109,7 +116,60 @@ class TaggingTest :
 				QVERIFY(trackNew.year() == 1995);
 				QVERIFY(trackNew.trackNumber() == 17);
 			}
-		};
+		}
+
+		// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+		[[maybe_unused]] void testWriteOnlyChangedTracks()
+		{
+			struct TestCase
+			{
+				TestEnv testEnv;
+			};
+
+			const auto testCases = std::array {
+				TestCase {{":/test/mp3test.mp3", tempPath("sayonara-test.mp3")}},
+				TestCase {{":/test/oggtest.ogg", tempPath("sayonara-test.ogg")}},
+				TestCase {{":/test/wavtest.wav", tempPath("wavRiff.wav")}},
+				TestCase {{":/test/emptyTestFile.mp3", tempPath("emptyTestFile.mp3")}},
+				TestCase {{":/test/mp4test.mp4", tempPath("sayonara-test.mp4")}}
+			};
+
+			for(const auto setTagsFirst: {true, false})
+			{
+				for(const auto& testCase: testCases)
+				{
+					const auto track = setTagsFirst
+					                   ? createBasicTrack(testCase.testEnv.filename())
+					                   : MetaData {testCase.testEnv.filename()};
+					const auto written = Tagging::Utils::setMetaDataOfFile(track);
+					QVERIFY(written == true);
+
+					auto updatedTrack = track;
+					updatedTrack.setArtist("artist2");
+					updatedTrack.setAlbum("album2");
+					updatedTrack.setGenres(QStringList() << "genre3" << "genre4");
+					updatedTrack.setTitle("title2");
+					updatedTrack.setComment("comment2");
+					updatedTrack.setYear(1996);
+					updatedTrack.setTrackNumber(18);
+
+					const auto writtenAgain = Tagging::Utils::setOnlyChangedMetaDataOfFile(track, updatedTrack);
+					QVERIFY(writtenAgain);
+
+					auto trackReloaded = MetaData(testCase.testEnv.filename());
+					Tagging::Utils::getMetaDataOfFile(trackReloaded);
+					QVERIFY(trackReloaded.artist() == "artist2");
+					QVERIFY(trackReloaded.album() == "album2");
+					QVERIFY(trackReloaded.genres().count() == 2);
+					QVERIFY(trackReloaded.genres().contains(Genre("genre3")));
+					QVERIFY(trackReloaded.genres().contains(Genre("genre4")));
+					QVERIFY(trackReloaded.title() == "title2");
+					QVERIFY(trackReloaded.comment() == "comment2");
+					QVERIFY(trackReloaded.year() == 1996);
+					QVERIFY(trackReloaded.trackNumber() == 18);
+				}
+			}
+		}
 };
 
 QTEST_GUILESS_MAIN(TaggingTest)
