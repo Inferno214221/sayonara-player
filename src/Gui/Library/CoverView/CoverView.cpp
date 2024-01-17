fa6ecf9d00059fdbf199c1f8a789faa2453eb3c2
Michael Lugmair <sayonara-player@posteo.org>
2024-01-17 19:24:24 +0100

    Albums: add sorting action to cover view

--- src/Gui/Library/CoverView/CoverView.cpp
+++ src/Gui/Library/CoverView/CoverView.cpp
@@ -169,14 +169,16 @@ namespace Library
 		return {
 			ActionPair(Lang::Name, true, SortOrder::AlbumNameAsc),
 			ActionPair(Lang::Name, false, SortOrder::AlbumNameDesc),
-			ActionPair(Lang::Year, true, SortOrder::AlbumYearAsc),
-			ActionPair(Lang::Year, false, SortOrder::AlbumYearDesc),
 			ActionPair(Lang::Artist, true, SortOrder::ArtistNameAsc),
 			ActionPair(Lang::Artist, false, SortOrder::ArtistNameDesc),
+			ActionPair(Lang::Created, true, SortOrder::AlbumCreationDateAsc),
+			ActionPair(Lang::Created, false, SortOrder::AlbumCreationDateDesc),
+			ActionPair(Lang::Year, true, SortOrder::AlbumYearAsc),
+			ActionPair(Lang::Year, false, SortOrder::AlbumYearDesc),
+			ActionPair(Lang::Duration, true, SortOrder::AlbumDurationAsc),
+			ActionPair(Lang::Duration, false, SortOrder::AlbumDurationDesc),
 			ActionPair(Lang::NumTracks, true, SortOrder::AlbumTracksAsc),
 			ActionPair(Lang::NumTracks, false, SortOrder::AlbumTracksDesc),
-			ActionPair(Lang::Duration, true, SortOrder::AlbumDurationAsc),
-			ActionPair(Lang::Duration, false, SortOrder::AlbumDurationDesc)
 		};
 	}
 
