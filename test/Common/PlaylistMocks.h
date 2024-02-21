08169aa41be617d1221b81a0e3d63a232e5eaccb
Michael Lugmair <sayonara-player@posteo.org>
2024-02-21 12:28:04 +0100

    Test: add PlaylistHandlerMock

--- test/Common/PlaylistMocks.h
+++ test/Common/PlaylistMocks.h
@@ -20,9 +20,14 @@
 #ifndef SAYONARA_PLAYER_PLAYLISTMOCKS_H
 #define SAYONARA_PLAYER_PLAYLISTMOCKS_H
 
-#include "Utils/Playlist/CustomPlaylist.h"
-#include "Components/Playlist/PlaylistLoader.h"
+#include "Common/PlayManagerMock.h"
+
+#include "Components/Playlist/Playlist.h"
 #include "Components/Playlist/PlaylistHandler.h"
+#include "Components/Playlist/PlaylistLoader.h"
+#include "Utils/Algorithm.h"
+#include "Utils/MetaData/MetaDataList.h"
+#include "Utils/Playlist/CustomPlaylist.h"
 
 #include <QList>
 
@@ -32,22 +37,106 @@ class PlaylistLoaderMock :
 		QList<CustomPlaylist> m_playlists;
 
 	public:
-		int getLastPlaylistIndex() const override
-		{
-			return -1;
-		}
+		[[nodiscard]] int getLastPlaylistIndex() const override { return -1; }
 
-		int getLastTrackIndex() const override
-		{
-			return -1;
-		}
+		[[nodiscard]] int getLastTrackIndex() const override { return -1; }
 
-		const QList<CustomPlaylist>& playlists() const override
-		{
-			return m_playlists;
-		}
+		[[nodiscard]] const QList<CustomPlaylist>& playlists() const override { return m_playlists; }
 };
 
-using PlaylistHandlerMock = Playlist::Handler;
+class PlaylistHandlerMock :
+	public PlaylistAccessor,
+	public PlaylistCreator
+{
+	public:
+		explicit PlaylistHandlerMock(std::shared_ptr<PlayManagerMock> playManager) :
+			m_playManager {std::move(playManager)} {}
+
+		~PlaylistHandlerMock() override = default;
+
+		[[nodiscard]] int activeIndex() const override { return m_activeIndex; }
+
+		void setActiveIndex(const int index) { m_activeIndex = index; }
+
+		PlaylistPtr activePlaylist() override { return m_playlists[m_activeIndex]; }
+
+		[[nodiscard]] int currentIndex() const override { return m_currentIndex; };
+
+		void setCurrentIndex(const int index) override { m_currentIndex = index; }
+
+		PlaylistPtr playlist(const int index) override { return m_playlists[index]; };
+
+		PlaylistPtr playlistById(const int id) override
+		{
+			const auto it = Util::Algorithm::find(m_playlists, [&](const PlaylistPtr& playlist) {
+				return playlist->id() == id;
+			});
+
+			return it != m_playlists.end() ? *it : nullptr;
+		}
+
+		[[nodiscard]] int count() const override { return m_playlists.count(); }
+
+		[[nodiscard]] QString requestNewPlaylistName(const QString& prefix) const override
+		{
+			static int i = 0;
+			return prefix + QString::number(++i);
+		}
+
+		int createPlaylist(const MetaDataList& tracks, const QString& name, bool temporary, bool isLocked) override
+		{
+			auto playlist = std::make_shared<Playlist::Playlist>(m_playlists.count(), name, m_playManager.get());
+			playlist->createPlaylist(tracks);
+			playlist->setTemporary(temporary);
+			playlist->setLocked(isLocked);
+			m_playlists << playlist;
+
+			if(m_currentIndex < 0)
+			{
+				setCurrentIndex(playlist->index());
+			}
+
+			if(m_activeIndex < 0)
+			{
+				setActiveIndex(playlist->index());
+			}
+
+			return playlist->index();
+		}
+
+		int createPlaylist(const QStringList& paths, const QString& name, bool temporary,
+		                   Playlist::LocalPathPlaylistCreator* /*playlistFromPathCreator*/) override
+		{
+			auto tracks = MetaDataList {};
+			Util::Algorithm::transform(paths, tracks, [](const auto& path) {
+				return MetaData {path};
+			});
+
+			return createPlaylist(tracks, name, temporary, false);
+		}
+
+		int createPlaylist(const CustomPlaylist& customPlaylist) override
+		{
+			return createPlaylist(customPlaylist.tracks(), customPlaylist.name(),
+			                      customPlaylist.isTemporary(), customPlaylist.isLocked());
+		}
+
+		int createEmptyPlaylist(const bool /*override*/) override
+		{
+			return createPlaylist(MetaDataList {}, requestNewPlaylistName({}), true, false);
+		}
+
+		int createCommandLinePlaylist(const QStringList& pathList,
+		                              Playlist::LocalPathPlaylistCreator* playlistFromPathCreator) override
+		{
+			return createPlaylist(pathList, "command-line", true, playlistFromPathCreator);
+		}
+
+	private:
+		QList<PlaylistPtr> m_playlists;
+		int m_currentIndex {-1};
+		int m_activeIndex {-1};
+		std::shared_ptr<PlayManager> m_playManager;
+};
 
 #endif //SAYONARA_PLAYER_PLAYLISTMOCKS_H
