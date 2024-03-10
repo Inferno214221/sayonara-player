66c725ba0970ddbfca37071569352cc9dab55cbf
Michael Lugmair <sayonara-player@posteo.org>
2024-03-10 20:06:49 +0100

    LibraryDatabase: Refactor test

--- test/Database/LibraryDatabaseTest.cpp
+++ test/Database/LibraryDatabaseTest.cpp
@@ -21,25 +21,80 @@
 #include "test/Common/SayonaraTest.h"
 
 #include "Database/Connector.h"
-#include "Database/LibraryDatabase.h"
 #include "Database/Library.h"
-#include "Utils/MetaData/MetaDataList.h"
+#include "Database/LibraryDatabase.h"
+#include "Database/Query.h"
+#include "Utils/Algorithm.h"
+#include "Utils/FileUtils.h"
 #include "Utils/MetaData/Album.h"
 #include "Utils/MetaData/Artist.h"
-#include "Utils/MetaData/MetaDataSorting.h"
+#include "Utils/MetaData/MetaDataList.h"
 #include "Utils/Utils.h"
-#include "Utils/FileUtils.h"
 
 #include <QMap>
+
+namespace
+{
+	constexpr const auto libraryId = 0;
+	constexpr const auto databaseId = 0;
+	struct TestEnv
+	{
+		TestEnv()
+		{
+			cleanup();
+
+			DB::Connector::instance()->registerLibraryDatabase(libraryId);
+
+			// NOLINTNEXTLINE(*-prefer-member-initializer)
+			libraryDatabase = DB::Connector::instance()->libraryDatabase(libraryId, databaseId);
+		}
+
+		~TestEnv()
+		{
+			cleanup();
+		}
+
+		void cleanup()
+		{
+			const auto q1 = DB::Connector::instance()->runQuery("DELETE FROM tracks;", "Cannot delete all tracks");
+			QVERIFY(!DB::hasError(q1));
+
+			const auto q2 = DB::Connector::instance()->runQuery("DELETE FROM albums;", "Cannot delete all albums");
+			QVERIFY(!DB::hasError(q2));
+
+			const auto q3 = DB::Connector::instance()->runQuery("DELETE FROM artists;", "Cannot delete all artists");
+			QVERIFY(!DB::hasError(q3));
+
+			const auto q4 = DB::Connector::instance()->runQuery("DELETE FROM Libraries;",
+			                                                    "Cannot delete all libraries");
+			QVERIFY(!DB::hasError(q4));
+		}
+
+		DB::LibraryDatabase* libraryDatabase {nullptr};
+	};
+
+	MetaData createTrack(const QString& artist, const QString& album, const QString& albumArtist, const QString& title)
+	{
+		const auto filepath = QString("/%1/%2/%3/%4%5.mp3")
+			.arg(albumArtist)
+			.arg(artist)
+			.arg(album)
+			.arg(title);
+
+		auto track = MetaData {filepath};
+		track.setArtist(artist);
+		track.setAlbumArtist(albumArtist);
+		track.setAlbum(album);
+		track.setTitle(title);
+
+		return track;
+	}
+}
 
 class LibraryDatabaseTest :
 	public Test::Base
 {
 	Q_OBJECT
-
-	private:
-		QStringList mAlbumNames;
-		DB::LibraryDatabase* mLibraryDatabase = nullptr;
 
 	public:
 		LibraryDatabaseTest() :
@@ -47,143 +102,158 @@ class LibraryDatabaseTest :
 
 		~LibraryDatabaseTest() override = default;
 
-	private:
-		DB::LibraryDatabase* initDatabase();
-
 	private slots:
-		void testStore();
+
+		// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+		[[maybe_unused]] void testInsertTrackCreatesArtists()
+		{
+			[[maybe_unused]] auto env = TestEnv {};
+
+			struct TestCase
+			{
+				MetaData track;
+				QStringList expectedArtists;
+			};
+
+			const auto testCases = std::array {
+				TestCase {createTrack("artist", "a", "albumArtist", "t1"), {"albumArtist", "artist"}},
+				TestCase {createTrack("artist", "a", "albumArtist", "t2"), {"albumArtist", "artist"}},
+				TestCase {createTrack("artist2", "a", "albumArtist", "t3"), {"albumArtist", "artist", "artist2"}},
+				TestCase {createTrack("artist3", "a", "albumArtist", "t4"),
+				          {"albumArtist", "artist", "artist2", "artist3"}}
+			};
+
+			for(const auto& testCase: testCases)
+			{
+				const auto tracks = MetaDataList() << testCase.track;
+				env.libraryDatabase->storeMetadata(tracks);
+
+				auto artists = ArtistList {};
+				env.libraryDatabase->getAllArtists(artists, true);
+
+				QCOMPARE(artists.count(), testCase.expectedArtists.count());
+				for(const auto& name: testCase.expectedArtists)
+				{
+					const auto contains = Util::Algorithm::contains(artists, [&](const auto& artist) {
+						return (artist.name() == name);
+					});
+
+					QVERIFY(contains);
+				}
+			}
+		}
+
+		// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+		[[maybe_unused]] void testInsertTrackCreatesAlbums()
+		{
+			[[maybe_unused]] auto env = TestEnv {};
+
+			struct TestCase
+			{
+				MetaData track;
+				QList<QPair<QString, QString>> expectedAlbums;
+			};
+
+			const auto testCases = std::array {
+				TestCase {createTrack("a", "album1", "a", "t1"), {{"album1", "a"}}},
+				TestCase {createTrack("a", "album1", "a", "t2"), {{"album1", "a"}}},
+				TestCase {createTrack("a", "album2", "a", "t3"), {{"album1", "a"}, {"album2", "a"}}},
+				TestCase {createTrack("a", "album3", "a", "t4"), {{"album1", "a"}, {"album2", "a"}, {"album3", "a"}}},
+				TestCase {createTrack("a", "album1", "other", "t5"),
+				          {{"album1", "a"}, {"album1", "other"}, {"album2", "a"}, {"album3", "a"}}},
+			};
+
+			for(const auto& testCase: testCases)
+			{
+				const auto tracks = MetaDataList() << testCase.track;
+				env.libraryDatabase->storeMetadata(tracks);
+
+				auto albums = AlbumList {};
+				env.libraryDatabase->getAllAlbums(albums, true);
+
+				QCOMPARE(albums.count(), testCase.expectedAlbums.count());
+				for(const auto& [name, albumArtist]: testCase.expectedAlbums)
+				{
+					const auto contains =
+						Util::Algorithm::contains(albums, [n = name, a = albumArtist](const auto& album) {
+							return (album.name() == n) && (album.albumArtist() == a);
+						});
+
+					QVERIFY(contains);
+				}
+			}
+		}
+
+		// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+		[[maybe_unused]] void testInsertTracksAtOnceCreatesAlbums()
+		{
+			[[maybe_unused]] auto env = TestEnv {};
+
+			const auto tracks = MetaDataList()
+				<< createTrack("a", "album1", "a", "t1")
+				<< createTrack("a", "album1", "a", "t2")
+				<< createTrack("a", "album2", "a", "t3")
+				<< createTrack("a", "album3", "a", "t4")
+				<< createTrack("a", "album1", "other", "t5");
+
+			const auto expectedAlbums = QList<QPair<QString, QString>> {
+				{{"album1", "a"}, {"album1", "other"}, {"album2", "a"}, {"album3", "a"}}
+			};
+
+			env.libraryDatabase->storeMetadata(tracks);
+
+			auto albums = AlbumList {};
+			env.libraryDatabase->getAllAlbums(albums, true);
+
+			QCOMPARE(albums.count(), expectedAlbums.count());
+			for(const auto& [name, albumArtist]: expectedAlbums)
+			{
+				const auto contains =
+					Util::Algorithm::contains(albums, [n = name, a = albumArtist](const auto& album) {
+						return (album.name() == n) && (album.albumArtist() == a);
+					});
+
+				QVERIFY(contains);
+			}
+		}
+
+		// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+		[[maybe_unused]] void testInsertTrackSetsArtistAndAlbumId()
+		{
+			[[maybe_unused]] auto env = TestEnv {};
+
+			struct TestCase
+			{
+				MetaData track;
+			};
+
+			const auto testCases = std::array {
+				TestCase {createTrack("ar1", "al1", "aa1", "t1")},
+				TestCase {createTrack("ar1", "al2", "aa2", "t2")},
+				TestCase {createTrack("ar1", "al3", "aa3", "t3")}
+			};
+
+			for(const auto& testCase: testCases)
+			{
+				const auto tracks = MetaDataList() << testCase.track;
+				env.libraryDatabase->storeMetadata(tracks);
+
+				auto fetchedTracks = MetaDataList {};
+				env.libraryDatabase->getAllTracks(fetchedTracks);
+
+				const auto it = Util::Algorithm::find(fetchedTracks, [&](const auto& track) {
+					return testCase.track.isEqual(track);
+				});
+
+				QVERIFY(it->artist() == testCase.track.artist());
+				QVERIFY(it->artistId() >= 0);
+				QVERIFY(it->album() == testCase.track.album());
+				QVERIFY(it->albumId() >= 0);
+				QVERIFY(it->albumArtist() == testCase.track.albumArtist());
+				QVERIFY(it->albumArtistId() >= 0);
+			}
+		}
 };
-
-static MetaDataList createTracks()
-{
-	MetaDataList tracks;
-	for(int i = 0; i < 100; i++)
-	{
-		MetaData md;
-
-		md.setFilepath(QString("/path/to/file%1.mp3").arg(i));
-		md.setArtist(QString("Artist%1").arg(i / 20));
-		md.setAlbum(QString("Album%1").arg(i / 10));
-		md.setTitle(QString("Title%1").arg(i));
-		md.setTrackNumber(i % 10);
-		md.setYear(2000 + i / 20);
-		md.setBitrate(Util::randomNumber(0, 320000));
-
-		tracks << md;
-	}
-
-	// track[00-09]: artist0, album0
-	// track[10-19]: artist0, album1
-	// track[20-29]: artist1, album2
-	// track[30-39]: artist1, album3
-	// track[40-49]: artist2, album4
-	// track[50-59]: artist2, album5
-	// track[60-69]: artist3, album6
-	// track[70-79]: artist3, album7
-	// track[80-89]: artist4, album8
-	// track[90-99]: artist4, album9
-
-	MetaDataSorting::sortMetadata(tracks, Library::TrackSortorder::TitleAsc);
-	return tracks;
-}
-
-void LibraryDatabaseTest::testStore()
-{
-	const MetaDataList tracks = createTracks();
-	QVERIFY(tracks.count() == 100);
-
-	auto* libraryDb = initDatabase();
-	bool success = libraryDb->storeMetadata(tracks);
-
-	QVERIFY(success == true);
-
-	int trackcount = libraryDb->getNumTracks();
-	qDebug() << "trackcount " << trackcount;
-	QVERIFY(trackcount == 100);
-
-	MetaDataList tempTracks;
-	success = libraryDb->getAllTracks(tempTracks);
-	QVERIFY(success == true);
-	QVERIFY(tempTracks.count() == tracks.count());
-
-	MetaDataSorting::sortMetadata(tempTracks, Library::TrackSortorder::TitleAsc);
-	for(int i = 0; i < tracks.count(); i++)
-	{
-		QVERIFY(tracks[i] == tempTracks[i]);
-	}
-
-	AlbumList albums;
-	libraryDb->getAllAlbums(albums, true);
-	QVERIFY(albums.count() == 10);
-
-	ArtistList artists;
-	libraryDb->getAllArtists(artists, true);
-	QVERIFY(artists.count() == 5);
-
-	MetaData md1 = tracks[0];
-	md1.setArtist("Some new Artist");
-
-	MetaData md2 = tracks[0];
-	md2.setArtist("");
-	md2.setAlbum("");
-	md2.setFilepath("/a/completely/different/path.mp3");
-
-	tempTracks.clear();
-	tempTracks << md1 << md2;
-
-	QVERIFY(tempTracks.count() == 2);
-	success = libraryDb->storeMetadata(tempTracks);
-	QVERIFY(success == true);
-
-	trackcount = libraryDb->getNumTracks();
-	QVERIFY(trackcount == tracks.count() + 1);
-
-	success = libraryDb->getAllArtists(artists, true);
-	QVERIFY(success);
-	QVERIFY(artists.count() == 7);
-
-	success = libraryDb->getAllAlbums(albums, true);
-	QVERIFY(success);
-	QVERIFY(albums.count() == 12);
-
-	success = libraryDb->getAllTracks(tempTracks);
-	qDebug() << "New trackcount " << tempTracks.count();
-	QVERIFY(success);
-	QVERIFY(tempTracks.count() == trackcount);
-
-	libraryDb->storeMetadata(tracks);
-	trackcount = libraryDb->getNumTracks();
-	QVERIFY(trackcount == tracks.count() + 1);
-
-	libraryDb->getAllArtists(artists, true);
-	QVERIFY(artists.count() == 7);
-
-	libraryDb->getAllAlbums(albums, true);
-	QVERIFY(albums.count() == 12);
-
-	// md1 is overwritten again -> artist of md1 has no tracks anymore
-	// md2 stays as the filepath differs -> artist of md2 stays
-	libraryDb->getAllArtists(artists, false);
-	QVERIFY(artists.count() == 6);
-
-	// album of md2 stays
-	libraryDb->getAllAlbums(albums, false);
-	QVERIFY(albums.count() == 11);
-}
-
-DB::LibraryDatabase* LibraryDatabaseTest::initDatabase()
-{
-	if(mLibraryDatabase)
-	{
-		return mLibraryDatabase;
-	}
-
-	auto* db = DB::Connector::instance();
-	db->registerLibraryDatabase(0);
-	mLibraryDatabase = db->libraryDatabase(0, 0);
-
-	return mLibraryDatabase;
-}
 
 QTEST_GUILESS_MAIN(LibraryDatabaseTest)
 
